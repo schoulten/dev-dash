965c34f4bd7af1f218f5c24cf7efbc2df1bb35af
Fernando da Silva <fernando.schoulten@hotmail.com>
2023-06-12 22:11:49 +0000

    Criar barra lateral na UI

--- pd08/app.py
+++ pd08/app.py
@@ -3,7 +3,28 @@ from shiny import ui, App
 
 # Interface do usuário ----
 app_ui = ui.page_navbar(
-    ui.nav("Página 1"),
+    ui.nav(
+        "Página 1",
+        ui.layout_sidebar(
+            ui.panel_sidebar(
+                ui.markdown(
+                    """
+                    Um texto em **negrito** ou em *itálico*.
+
+                    Um parágrafo com um [link](https://analisemacro.com.br/).
+
+                    - Item 1
+                    - Item 2
+                    - Item 3
+
+                    ![](https://aluno.analisemacro.com.br/wp-content/uploads/dlm_uploads/2023/05/logo_am_45.png)
+                    """
+                ),
+            width = 3
+            ),
+            ui.panel_main()
+        )
+        ),
     ui.nav("Página 2"),
     ui.nav_control(ui.a("Análise Macro", href = "https://analisemacro.com.br/")),
     ui.nav_menu(
