fb93a7182e6c1f69a4226380d582c2e7a6cc72a9
Fernando da Silva <fernando.schoulten@hotmail.com>
2024-10-01 19:46:16 +0000

    Adiciona elementos do painel principal da UI

--- pd08/app.py
+++ pd08/app.py
@@ -23,6 +23,25 @@ app_ui = ui.page_navbar(
             "Painel principal",
             ui.markdown("Texto *markdown*"),
             ui.img(src = "https://aluno.analisemacro.com.br/wp-content/uploads/dlm_uploads/2023/05/logo_am_45.png", width = 45),
+            ui.row(
+                ui.column(6, "Linha 1 - Coluna A", style = "background-color: blue;"),
+                ui.column(6, "Linha 1 - Coluna B", style = "background-color: red;")
+                ),
+            ui.row(
+                ui.column(5, "Linha 2 - Coluna A", style = "background-color: yellow;"),                
+                ui.column(2, "Linha 2 - Coluna B", style = "background-color: orange;"),
+                ui.column(5, "Linha 2 - Coluna C", style = "background-color: purple;")
+                ),
+            ui.layout_columns(
+                ui.card(ui.card_header("Título"), "Gráfico 1"),
+                ui.card("Tabela 1")
+                ),
+            
+            ui.layout_columns(
+                ui.card(ui.card_header("Título"), "Gráfico 2"),
+                ui.card("Tabela 2")
+                ),
+            ui.card(ui.card_header("Título"), "Gráfico 3"),
             bg = "#e8e8e8"
             )
         ),
